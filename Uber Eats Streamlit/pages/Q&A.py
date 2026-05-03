caa31295bbe73fc56981b6f217c219e293628139
Vishaul <147958439+Vishaul18@users.noreply.github.com>
2026-05-03 08:18:35 +0000

    Added Q&A page with 10 SQL queeries

--- Uber Eats Streamlit/pages/Q&A.py
+++ Uber Eats Streamlit/pages/Q&A.py
@@ -1,4 +1,117 @@
 import streamlit as st
+import sqlite3
+import pandas as pd
 
-st.title("Q&A Page")
-st.write("Business questions here")
+st.title("Business Q&A")
+
+conn = sqlite3.connect("uber.db")
+
+question = st.selectbox("Select a Business Question", [
+    "1. Highest Avg Rating Locations",
+    "2. Most Saturated Locations",
+    "3. Online Order Impact",
+    "4. Table Booking Impact",
+    "5. Best Price Category",
+    "6. Price Category vs Rating",
+    "7. Most Common Cuisines",
+    "8. Highest Rated Cuisines",
+    "9. Niche Cuisines (Low count, high rating)",
+    "10. Cost vs Rating"
+])
+if question == "1. Highest Avg Rating Locations":
+    query = """
+    SELECT location, ROUND(AVG(normalized_rating),2) as avg_rating
+    FROM restaurants
+    GROUP BY location
+    ORDER BY avg_rating DESC
+    """
+
+elif question == "2. Most Saturated Locations":
+    query = """
+    SELECT location, COUNT(*) as total
+    FROM restaurants
+    GROUP BY location
+    ORDER BY total DESC
+    """
+
+elif question == "3. Online Order Impact":
+    query = """
+    SELECT online_order, ROUND(AVG(normalized_rating),2) as avg_rating
+    FROM restaurants
+    GROUP BY online_order
+    """
+
+elif question == "4. Table Booking Impact":
+    query = """
+    SELECT book_table, ROUND(AVG(normalized_rating),2) as avg_rating
+    FROM restaurants
+    GROUP BY book_table
+    """
+
+elif question == "5. Best Price Category":
+    query = """
+    SELECT 
+    CASE 
+        WHEN cost_for_two < 300 THEN 'Low'
+        WHEN cost_for_two BETWEEN 300 AND 700 THEN 'Mid'
+        ELSE 'High'
+    END AS price_category,
+    ROUND(AVG(normalized_rating),2) as avg_rating
+    FROM restaurants
+    GROUP BY price_category
+    ORDER BY avg_rating DESC
+    """
+
+elif question == "6. Price Category vs Rating":
+    query = """
+    SELECT 
+    CASE 
+        WHEN cost_for_two < 300 THEN 'Low'
+        WHEN cost_for_two BETWEEN 300 AND 700 THEN 'Mid'
+        ELSE 'High'
+    END AS price_category,
+    COUNT(*) as total,
+    ROUND(AVG(normalized_rating),2) as avg_rating
+    FROM restaurants
+    GROUP BY price_category
+    """
+
+elif question == "7. Most Common Cuisines":
+    query = """
+    SELECT cuisines, COUNT(*) as total
+    FROM restaurants
+    GROUP BY cuisines
+    ORDER BY total DESC
+    LIMIT 10
+    """
+
+elif question == "8. Highest Rated Cuisines":
+    query = """
+    SELECT cuisines, ROUND(AVG(normalized_rating),2) as avg_rating
+    FROM restaurants
+    GROUP BY cuisines
+    ORDER BY avg_rating DESC
+    LIMIT 10
+    """
+
+elif question == "9. Niche Cuisines (Low count, high rating)":
+    query = """
+    SELECT cuisines, COUNT(*) as total, ROUND(AVG(normalized_rating),2) as avg_rating
+    FROM restaurants
+    GROUP BY cuisines
+    HAVING total < 50
+    ORDER BY avg_rating DESC
+    LIMIT 10
+    """
+
+elif question == "10. Cost vs Rating":
+    query = """
+    SELECT cost_for_two, ROUND(AVG(normalized_rating),2) as avg_rating
+    FROM restaurants
+    GROUP BY cost_for_two
+    ORDER BY cost_for_two
+    """
+df = pd.read_sql(query, conn)
+
+st.subheader("Result")
+st.dataframe(df)
--- Uber Eats Streamlit/pages/dashboard.py
+++ Uber Eats Streamlit/pages/dashboard.py
@@ -1,4 +1,65 @@
 import streamlit as st
+import sqlite3
+import pandas as pd
 
-st.title("Dashboard")
-st.write("This is dashboard page")
+st.title("Uber Eats Decision Dashboard")
+
+conn = sqlite3.connect("uber.db")
+# -------------------
+# LOAD FILTER VALUES
+#-------------------
+# SQ# -------------------
+
+locations = pd.read_sql("SELECT DISTINCT location FROM restaurants", conn)['location']
+location = st.sidebar.selectbox("Location", locations)
+online = st.sidebar.selectbox("Online Order", ["yes", "no"])
+table = st.sidebar.selectbox("Table Booking", ["yes", "no"])
+
+# QUERY
+# -------------------
+
+query = f"SELECT * FROM restaurants WHERE location = '{location}' AND online_order = '{online}' AND book_table ='{table}'"
+df = pd.read_sql(query, conn)
+
+# -------------------
+# OUTPUT
+# -------------------
+
+st.subheader("Filtered Restaurants")
+st.write(f"Rows returned: {len(df)}")  # debug
+st.dataframe(df)
+
+# -------------------
+# INSIGHTS
+# -------------------
+
+st.subheader("Location Intelligence")
+loc_df = pd.read_sql("""
+SELECT location, ROUND(AVG(normalized_rating),2) as avg_rating, COUNT(*) as total
+FROM restaurants
+GROUP BY location
+ORDER BY avg_rating DESC
+""", conn)
+st.dataframe(loc_df)
+
+st.subheader("Pricing Optimization")
+price_df = pd.read_sql("""
+SELECT 
+CASE 
+    WHEN cost_for_two < 300 THEN 'Low'
+    WHEN cost_for_two BETWEEN 300 AND 700 THEN 'Mid'
+    ELSE 'High'
+END AS price_category,
+ROUND(AVG(normalized_rating),2) as avg_rating
+FROM restaurants
+GROUP BY price_category
+""", conn)
+st.dataframe(price_df)
+
+st.subheader("Feature Impact")
+feature_df = pd.read_sql("""
+SELECT online_order, ROUND(AVG(normalized_rating),2) as avg_rating
+FROM restaurants
+GROUP BY online_order
+""", conn)
+st.dataframe(feature_df)
