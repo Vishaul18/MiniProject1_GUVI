3c71b6e5d73f6c869a11fa67df89b2d6195c53c1
Vishaul <147958439+Vishaul18@users.noreply.github.com>
2026-05-03 03:10:48 +0000

    Added multipage app

---
+++ Uber Eats Streamlit/pages/Order_analysis.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Order Analysis")
+st.write("Order insights here")
---
+++ Uber Eats Streamlit/pages/Q&A.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Q&A Page")
+st.write("Business questions here")
